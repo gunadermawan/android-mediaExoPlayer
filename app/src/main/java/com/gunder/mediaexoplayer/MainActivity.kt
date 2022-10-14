eb64693504be846ac181df004be56132504bc0ce
itselaras <gunadermawan62@gmail.com>
2022-10-14 13:12:46 +0700

    create basics of exoplayer

--- app/src/main/java/com/gunder/mediaexoplayer/MainActivity.kt
+++ app/src/main/java/com/gunder/mediaexoplayer/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.gunder.mediaexoplayer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.gunder.mediaexoplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    companion object {
+        const val VIDEO_URL =
+            "https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(viewBinding.root)
+
+        val player = ExoPlayer.Builder(this).build()
+        viewBinding.videView.player = player
+
+        val mediaItem = MediaItem.fromUri(VIDEO_URL)
+        player.setMediaItem(mediaItem)
+        player.prepare()
     }
+
+
 }
